3c3cc68baeee75fc53c286d78b71fe6446aa47be
mduran007 <mdrduran@gmail.com>
2019-11-12 10:59:46 -0300

    node express typescript

---
+++ toten.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var toten = express();
+var path = require("path");
+var http = require('http').Server(toten);
+
+const bodyParser = require('body-parser');
+const pathToStaticFiles = path.join('client_public');
+console.log(pathToStaticFiles);
+
+const middlewares = [
+    express.static(path.join('client_public')),
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true })
+];
+
+toten.use(middlewares);
+
+toten.get('/', function (req, res) {
+    console.log(path.resolve(__dirname + '-/public/index.html'));
+    res.sendFile(path.resolve(__dirname + '/public/index.html'));
+});
+
+toten.listen(3000, function () {
+    console.log('toten on port 3000!');
+});
+
+
+toten.use(express.static(__dirname + '/public/css/'));
+toten.use(express.static(__dirname + '/public/img/'));
