33df0c8f57d349fb9e3a67b23aff2c12c8ed212b
wagnemarques <wagnerdocri@gmail.com>
2020-09-04 14:11:06 -0300

    basic express project structure

--- src/server/server-express.js
+++ src/server/server-express.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let appExpress = express();
+const PORT = process.env.PORT || 5000
 let path = require("path");
 let fs = require("fs");
 let inspect = require('object-inspect');
@@ -15,16 +16,13 @@ const middlewares = [
 ];
 
 //appExpress.use(express.static());
-
 appExpress.use(middlewares);
 
 delete(appExpress.cache);
 
 //https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
 //Heroku dynamically assigns your app a port, so you can't set the port to a fixed number. Heroku adds the port to the env
-http.listen( process.env.PORT || 5000, function() {
-    let port= process.env.PORT || 5000;
-});
+http.listen( PORT => console.log("Listening on port ${ PORT }") )
 
 exports.webapp = appExpress;
 
