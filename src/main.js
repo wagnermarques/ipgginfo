e10baea83a4681257e7958e612eb875c6f44a586
wagnemarques <wagnerdocri@gmail.com>
2020-09-04 14:24:21 -0300

    basic express project structure

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 /*jslint es6:true*/
 let path = require("path");
-let webapp = require("./server/server-express.js").webapp;
+let webapp = require("./server/server.js").webapp;
 //var persistence = require("./server/firebase-features.js").persistence;
 
-
-webapp.get('/', function(req, res){
-    res.sendFile(path.resolve(__dirname + './../views/index.html'));
-});
 
